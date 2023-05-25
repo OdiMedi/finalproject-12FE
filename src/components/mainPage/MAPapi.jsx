c1ca4bcd0e535c956b733cffd773ef36c2860e20
stella0905 <o122233@naver.com>
2023-05-25 16:30:31 +0900

    [Design] 지도 API 여러개 마커 찍기 완료

--- src/components/mainPage/MAPapi.jsx
+++ src/components/mainPage/MAPapi.jsx
@@ -5,6 +5,44 @@ import locationIcon from '../../assets/locationIcon.png';
 const { kakao } = window;
 
 const MAPapi = () => {
+  const dummyList = [
+    {
+      name: '흥부약국',
+      lon: 126.85581079958143,
+      lat: 37.55496841887348,
+    },
+    {
+      name: '희망찬약국',
+      lon: 126.84676212183612,
+      lat: 37.531164294971674,
+    },
+    {
+      name: '화곡서울약국',
+      lon: 126.83671729194344,
+      lat: 37.54843456317784,
+    },
+    {
+      name: '화곡태평양약국',
+      lon: 126.8376196876418,
+      lat: 37.54414899562802,
+    },
+    {
+      name: '화창한약국',
+      lon: 126.84611739011669,
+      lat: 37.56081981514185,
+    },
+    {
+      name: '화평약국',
+      lon: 126.86100108709572,
+      lat: 37.53193095203476,
+    },
+    {
+      name: '휴베이스비타민약국',
+      lon: 126.837978157379,
+      lat: 37.5348879429263,
+    },
+  ];
+
   useEffect(() => {
     const container = document.getElementById('myMap');
     const options = {
@@ -23,15 +61,14 @@ const MAPapi = () => {
       imageOption
     );
 
-    // 마커 생성
-    const markerPosition = new kakao.maps.LatLng(37.541, 126.986);
-    const marker = new kakao.maps.Marker({
-      position: markerPosition,
-      image: markerImage,
+    // 마커 생성 및 지도에 추가
+    dummyList.forEach(lo => {
+      const marker = new kakao.maps.Marker({
+        position: new kakao.maps.LatLng(lo.lat, lo.lon),
+        image: markerImage,
+      });
+      marker.setMap(map);
     });
-
-    // 마커를 지도에 표시
-    marker.setMap(map);
   }, []);
 
   return <MapDiv id="myMap">MAPapi</MapDiv>;
