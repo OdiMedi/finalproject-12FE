261876814043c5da37133f35e93967d0273b6a62
stella0905 <o122233@naver.com>
2023-06-09 02:52:09 +0900

    머지하면서 지워진거 수정

--- src/components/BookMark.jsx
+++ src/components/BookMark.jsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
-import { useMutation } from 'react-query';
-import { useState } from 'react';
+import { useMutation, useQueryClient } from 'react-query';
+import { useState, useEffect } from 'react';
 import Cookies from 'js-cookie';
 import api from '../api/axios';
 
