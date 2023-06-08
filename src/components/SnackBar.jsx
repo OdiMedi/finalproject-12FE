bd8624cd27e27e94fe1a2b76dd7a35ebee041397
stella0905 <109677566+stella0905@users.noreply.github.com>
2023-06-09 02:49:35 +0900

    [Design] 미로그인시 북마크 snackBar추가 (#41)
    * [feat] 머지하다가 다 날라감. 새로 수정
    
    * [Design] 상세페이지 ui완료
    
    * [Feat] 카카오맵 내위치 받아서 마커찍기까지 완료
    
    * [Feat] 전체리스트 불러오기 API 연결
    
    * [Design] 메인페이지 ui수정 (주소간격, 약국명 글자크기)
    
    * [Design] 홈화면에서 약국검색 페이지로 navigate 연결
    
    * [Feat] 필터, 검색기능 API연결 중
    
    * [Design] 댓글작성 모달 UI완료
    
    * [feat] 필터, 검색 전체데이터 불러오는중 / 아직 실시간으로 데이터를 불러오지는 못하는 상태
    
    * [Feat] 상세페이지 API 연결 / 전체리스트필터, 상세페이지 map은 에러로 인해 수정중이라 주석처리해놓음
    
    * [feat] 검색, 필터 조회 완료
    
    * [Design] 내 위치 버튼 UI완료
    
    * [Fix] 머지 후 간단한 에러 수정
    
    * [feat] 카카오맵 수정중
    
    * 배포를 위해 테스트 맵 주석처리
    
    * 배포 테스트중 2
    
    * 배포테스트
    
    * 배포 테스트3
    
    * 배포테스트 마지막
    
    * [feat] 카카오맵 마우스오버이벤트 추가
    
    * 테스트
    
    * test1
    
    * 외국어 전체리스트 UI완료
    
    * [Design] 외국어가능 약국메인 페이지 UI 및 상세페이지 댓글제외한 UI완료
    
    * [feat]상세페이지 북마크 및 공휴일, 야간영업 뱃지 표지 # 으로 수정완료
    
    * [Design]외국어 페이지 #영업시간 추가
    
    * [Feat] 외국어 페이지 좋아요 추가
    
    * [Feat]외국어 가능 리스트 및 상세페이지 API연결
    
    * [Rename] MapApi.jsx 파일위치 mainPage폴더에서 commponents하위폴더로 이동
    
    * [Fix]받아오는 값이 [] 없을때 맵에 나는 에러 해결
    
    * [Feat]내위치 기반 약국 전체조회 API연결
    
    * [Design] header, layout CSS 수정
    
    * [Fix]공통 스타일 전역으로 분리 및 전역스타일파일, commentItem파일 comment폴더로 위치 변경
    
    * [Design]StoreMain컴포넌트에 아무값 없을때 메세지 추가
    
    * [Design] 상세페이지 북마크 노출순서 위로 변경
    
    * merge 전 push
    
    * [Design] 내위치 호버이벤트 추가
    
    * [Design] 상세페이지 공휴일,주말영업시간 토글
    
    * [Fix] 내위치 찍었다가 구 변경시 내위치값 비워서 API재요청 되도록 수정
    
    * [Design] 미로그인시 북마크 snackBar추가

---
+++ src/components/SnackBar.jsx
@@ -0,0 +1,66 @@
+import { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import snack from '../assets/loginSnack.png';
+
+import * as CSS from '../style/globalStyle';
+import infoIcon from '../assets/infoIcon.png';
+
+const SnackBar = () => {
+  const [isOpen, setIsOpen] = useState(true);
+
+  useEffect(() => {
+    if (isOpen) {
+      setTimeout(() => {
+        setIsOpen(false);
+      }, 3000);
+    }
+  }, [isOpen]);
+
+  return (
+    <>
+      {isOpen && (
+        <SnackWrapDiv>
+          <SnackDiv>
+            <CSS.InfoIconImg src={infoIcon} alt="" />
+            <p>로그인 후 이용해주세요.</p>
+          </SnackDiv>
+        </SnackWrapDiv>
+      )}
+    </>
+  );
+};
+
+export default SnackBar;
+
+const SnackWrapDiv = styled.div`
+  width: 100vw;
+  height: 100vh;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  position: fixed;
+  background-color: rgba(49, 49, 49, 0.4);
+  z-index: 10;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const SnackDiv = styled.div`
+  width: 450px;
+  height: 80px;
+  background-image: url(${snack});
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+
+  p {
+    color: white;
+  }
+`;
