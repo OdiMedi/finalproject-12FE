cd4087469ae0e5279b301d326ea60ef37fdfb8ba
stella0905 <109677566+stella0905@users.noreply.github.com>
2023-06-12 20:40:26 +0900

    [Feat] 수정하기, 저장하기 구분 (#49)
    * [feat] 머지하다가 다 날라감. 새로 수정
    
    * [Design] 상세페이지 ui완료
    
    * [Feat] 카카오맵 내위치 받아서 마커찍기까지 완료
    
    * [Feat] 전체리스트 불러오기 API 연결
    
    * [Design] 메인페이지 ui수정 (주소간격, 약국명 글자크기)
    
    * [Design] 홈화면에서 약국검색 페이지로 navigate 연결
    
    * [Feat] 필터, 검색기능 API연결 중
    
    * [Design] 댓글작성 모달 UI완료
    
    * [feat] 필터, 검색 전체데이터 불러오는중 / 아직 실시간으로 데이터를 불러오지는 못하는 상태
    
    * [Feat] 상세페이지 API 연결 / 전체리스트필터, 상세페이지 map은 에러로 인해 수정중이라 주석처리해놓음
    
    * [feat] 검색, 필터 조회 완료
    
    * [Design] 내 위치 버튼 UI완료
    
    * [Fix] 머지 후 간단한 에러 수정
    
    * [feat] 카카오맵 수정중
    
    * 배포를 위해 테스트 맵 주석처리
    
    * 배포 테스트중 2
    
    * 배포테스트
    
    * 배포 테스트3
    
    * 배포테스트 마지막
    
    * [feat] 카카오맵 마우스오버이벤트 추가
    
    * 테스트
    
    * test1
    
    * 외국어 전체리스트 UI완료
    
    * [Design] 외국어가능 약국메인 페이지 UI 및 상세페이지 댓글제외한 UI완료
    
    * [feat]상세페이지 북마크 및 공휴일, 야간영업 뱃지 표지 # 으로 수정완료
    
    * [Design]외국어 페이지 #영업시간 추가
    
    * [Feat] 외국어 페이지 좋아요 추가
    
    * [Feat]외국어 가능 리스트 및 상세페이지 API연결
    
    * [Rename] MapApi.jsx 파일위치 mainPage폴더에서 commponents하위폴더로 이동
    
    * [Fix]받아오는 값이 [] 없을때 맵에 나는 에러 해결
    
    * [Feat]내위치 기반 약국 전체조회 API연결
    
    * [Design] header, layout CSS 수정
    
    * [Fix]공통 스타일 전역으로 분리 및 전역스타일파일, commentItem파일 comment폴더로 위치 변경
    
    * [Design]StoreMain컴포넌트에 아무값 없을때 메세지 추가
    
    * [Design] 상세페이지 북마크 노출순서 위로 변경
    
    * merge 전 push
    
    * [Design] 내위치 호버이벤트 추가
    
    * [Design] 상세페이지 공휴일,주말영업시간 토글
    
    * [Fix] 내위치 찍었다가 구 변경시 내위치값 비워서 API재요청 되도록 수정
    
    * [Design] 미로그인시 북마크 snackBar추가
    
    * 머지하면서 지워진거 수정
    
    * [Fix] 내위치 후 구 변경되면 바로 안바뀌던 버그 수정
    
    * [Design] notice컴포넌트만 만들고 router에 추가해놓음
    
    * [Fix] 지도에서 마커클릭시 한국어, 외국어페이지 구별해서 상세페이지로 연결 완료
    
    * 배포 오류 재배포
    
    * [Design] 공지사항 리스트, 상세페이지 UI완료
    
    * [Feat] header에 고객센터-> 공지사항으로 변경 및 페이지 연결 해놓음
    
    * [Feat] 공지사항 작성컴포넌트만 만들어놓고 router에 추가 및 글 작성 버튼으로 연결시켜 놓음
    
    * [Design] 공지사항 작성 페이지 UI완료
    
    * [Feat] 수정하기, 저장하기 구분

--- src/page/WriteNotice.jsx
+++ src/page/WriteNotice.jsx
@@ -1,7 +1,129 @@
-import React from 'react';
+
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import styled from 'styled-components';
 
 const WriteNotice = () => {
-  return <div>WriteNotice</div>;
+  const [isEdit, setIsEdit] = useState(false);
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+  const location = useLocation();
+  useEffect(() => {
+    if (location.state !== null) {
+      console.log(location.state.existingWriting.title);
+      setIsEdit(!isEdit);
+      setTitle(location.state.existingWriting.title);
+      setContent(location.state.existingWriting.content);
+    }
+  }, []);
+  const titleChangeHandle = e => {
+    setTitle(e.target.value);
+  };
+  const contentChangeHandle = e => {
+    setContent(e.target.value);
+  };
+  const noticeSaveButtonHandler = () => {
+    if (isEdit) {
+      alert('수정하기');
+    } else {
+      alert('저장하기');
+    }
+  };
+  return (
+    <BackgroundMain>
+      <NoticeH1>공지사항</NoticeH1>
+      <TitleBoxDiv>
+        <p>제목</p>
+        <TitleInput value={title} onChange={titleChangeHandle} />
+      </TitleBoxDiv>
+      <TitleBoxDiv>
+        <p>내용</p>
+        <ContentTextarea value={content} onChange={contentChangeHandle} />
+      </TitleBoxDiv>
+      <ButtonPositionDiv>
+        <WriteButton onClick={noticeSaveButtonHandler}>
+          {isEdit ? '글 수정하기' : '글 작성하기'}
+        </WriteButton>
+      </ButtonPositionDiv>
+    </BackgroundMain>
+  );
 };
 
 export default WriteNotice;
+
+const BackgroundMain = styled.main`
+  margin-left: auto;
+  margin-right: auto;
+  height: 700px;
+  width: 1113px;
+`;
+const NoticeH1 = styled.h1`
+  font-weight: 800;
+  font-size: 32px;
+  line-height: 43px;
+  margin-top: 70px;
+  margin-bottom: 50px;
+`;
+
+const TitleBoxDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 56px;
+  margin-bottom: 21px;
+
+  font-weight: 700;
+  font-size: 20px;
+  line-height: 24px;
+`;
+const TitleInput = styled.input`
+  border: 2px solid #dadada;
+  border-radius: 5px;
+  width: 976px;
+  height: 26px;
+  padding-left: 20px;
+  padding-right: 20px;
+  padding-top: 5px;
+  padding-bottom: 5px;
+
+  font-size: 17px;
+  &:focus {
+    outline: none;
+  }
+`;
+const ContentTextarea = styled.textarea`
+  border: 2px solid #dadada;
+  border-radius: 5px;
+  width: 976px;
+  height: 480px;
+  padding: 20px;
+  resize: none;
+  font-size: 17px;
+
+  &:focus {
+    outline: none;
+  }
+`;
+const WriteButton = styled.button`
+  background-color: #fa5938;
+  border-radius: 10px;
+  border: none;
+  width: 140px;
+  height: 50px;
+
+  font-weight: 900;
+  font-size: 18px;
+  line-height: 34px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  color: #ffffff;
+  &:hover {
+    box-shadow: 3px 3px 2px rgba(175, 174, 183, 0.5);
+  }
+`;
+const ButtonPositionDiv = styled.div`
+  display: flex;
+  justify-content: flex-end;
+`;
+
