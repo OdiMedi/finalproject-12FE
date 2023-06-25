1e5bce607a7dde917a711ebf9e6499dfb0097af5
stella0905 <109677566+stella0905@users.noreply.github.com>
2023-06-25 19:20:46 +0900

    북마크 개수 안보이게 일단 수정:useQuery로 수정하는데 시간이 오래걸릴 것 같음 (#111)
    * 북마크 개수 안보이게 일단 수정:useQuery로 수정하는데 시간이 오래걸릴 것 같음

--- src/api/storeList.js
+++ src/api/storeList.js
@@ -1,27 +1,5 @@
 import api from './axios';
 
-const storeAllList = async () => {
-  try {
-    const response = await api.get('api/store');
-    return response.data;
-  } catch (error) {
-    console.log(error);
-    throw error;
-  }
-};
-
-// 메인페이지 필터, 검색 조회
-// const storeFilterList = async props => {
-//   try {
-//     const response = await api.get(
-//       `api/store/search?radius=1&page=${props.page}&size=20&latitude=${props.currentLatitude}&longitude=${props.currentLongitude}&storeName=${props.name}&gu=${props.gu}&open=${props.open}&holidayBusiness=${props.holidayBusiness}&nightBusiness=${props.nightBusiness}`
-//     );
-//     return response.data;
-//   } catch (error) {
-//     console.log(error);
-//     throw error;
-//   }
-// };
 const storeFilterList = async props => {
   try {
     const response = await api.get(
@@ -44,4 +22,4 @@ const inquiryStoreDetail = async props => {
   }
 };
 
-export { storeAllList, storeFilterList, inquiryStoreDetail };
+export { storeFilterList, inquiryStoreDetail };
